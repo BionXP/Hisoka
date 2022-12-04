6c02907c054c2dc39f39ba9800dd35d2c7aab312
BionXP <112658830+BionXP@users.noreply.github.com>
2022-12-04 14:04:38 +0800

    Update logger.py

--- YukkiMusic/utils/logger.py
+++ YukkiMusic/utils/logger.py
@@ -19,17 +19,17 @@ async def play_logs(message, streamtype):
         else:
             chatusername = "Private Group"
         logger_text = f"""
-**REYY X YUKKI PLAY LOG**
+📮 **ZEN PLAY LOG**
 
-**Chat:** {message.chat.title} [`{message.chat.id}`]
-**User:** {message.from_user.mention}
-**Username:** @{message.from_user.username}
-**User ID:** `{message.from_user.id}`
-**Chat Link:** {chatusername}
+💬 **ᴄʜᴀᴛ:** {message.chat.title} [`{message.chat.id}`]
+👨🏻‍🚀 **ᴜsᴇʀ:** {message.from_user.mention}
+🔖 **ᴜsᴇʀɴᴀᴍᴇ:** @{message.from_user.username}
+🆔 **ᴜsᴇʀ ɪᴅ:** `{message.from_user.id}`
+🔗 **ᴄʜᴀᴛ ʟɪɴᴋ:** {chatusername}
 
-**Query:** {message.text}
+🏷 **Query:** {message.text}
 
-**StreamType:** {streamtype}"""
+💽 **StreamType:** {streamtype}"""
         if message.chat.id != LOG_GROUP_ID:
             try:
                 await app.send_message(
